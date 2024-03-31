d862d5ab10ef440fd4a126b2b542e36db8bfb2d5
황준용 <wnsdyd3333@gmail.com>
2024-04-01 04:52:55 +0900

    feat: 링크 추가 인풋 컴포넌트

---
+++ src/components/folder/AddLinkInput.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+const AddLinkInput = () => {
+  return (
+    <section className=" pt-16 pb-24 bg-[#edf7ff] tablat:px-8">
+      <form className="flex justify-center items-center max-w-[800px] mx-auto py-4 px-5 bg-white rounded-2xl border-2 border-[#6d6afe] ">
+        <div className="flex justify-center items-center gap-3 flex-grow">
+          <Image src={'/assets/icons/link.svg'} width={20} height={20} alt="폴더 추가 인풋 아이콘" />
+          <input
+            className="flex-grow border-none leading-6 placeholder:text-[#9fa6b2]"
+            type="text"
+            placeholder="링크를 추가해 보세요."
+          />
+        </div>
+        <button className="btn w-20 py-2 px-4 text-sm">추가하기</button>
+      </form>
+    </section>
+  );
+};
+export default AddLinkInput;
