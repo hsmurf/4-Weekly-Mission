94a2342faa08d48f9af5a792cc54e7f98eb9b0a4
황준용 <wnsdyd3333@gmail.com>
2024-04-01 02:32:08 +0900

    feat: SearchBar 컴포넌트

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const SearchBar = () => {
+  return (
+    <div className="flex gap-3 w-full rounded-xl bg-[#f5f5f5] p-4">
+      <Image src={'/assets/icons/Search.svg'} width={20} height={20} alt="돋보기 아이콘" />
+      <input className="w-full bg-[#f5f5f5] placeholder:text-[#666]" type="text" placeholder="링크를 검색해 보세요." />
+    </div>
+  );
+};
+export default SearchBar;
