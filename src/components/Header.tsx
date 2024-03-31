79f593dbaf0a2757da085d45f9d43ce4cf61250d
황준용 <wnsdyd3333@gmail.com>
2024-03-31 23:49:51 +0900

    feat: header 컴포넌트

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <header className=" bg-blue-50 z-10">
+      <nav className="flex justify-between items-center w-full max-w-[1920px] h-24 px-52 md:px-8">
+        <Link href={'/'}>
+          <Image src="/assets/icons/logo.svg" width={100} height={100} alt="로고" />
+        </Link>
+        <div className="flex justify-center items-center gap-1.5">
+          <Image src={'/assets/icons/kakao.svg'} width={28} height={28} alt="프로필 이미지" />
+          <span className=" text-[14px] text-[#373740]">sss</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
+export default Header;
