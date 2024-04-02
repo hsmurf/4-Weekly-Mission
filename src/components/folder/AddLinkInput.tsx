e4173ac3cfb2de2236b13f1bc575405d72cbb315
황준용 <wnsdyd3333@gmail.com>
2024-04-02 12:48:39 +0900

    feat: props로 받은 거 이벤트 등록

--- src/components/folder/AddLinkInput.tsx
+++ src/components/folder/AddLinkInput.tsx
@@ -1,19 +1,30 @@
 import Image from 'next/image';
 import '@/app/globals.css';
-export default function AddLinkInput() {
+
+interface AddLinkInputProps {
+  openModal: (modalType: string) => void;
+  setModalFolderName: (name: string) => void;
+}
+export default function AddLinkInput({ openModal, setModalFolderName }: AddLinkInputProps) {
   return (
-    <section className=" pt-16 pb-24 bg-[#edf7ff] tablat:px-8">
-      <form className="flex justify-center items-center max-w-[800px] mx-auto py-4 px-5 bg-white border-purple  rounded-2xl">
-        <div className="flex justify-center items-center gap-3 flex-grow">
-          <Image src={'/assets/icons/link.svg'} width={20} height={20} alt="폴더 추가 인풋 아이콘" />
-          <input
-            className="flex-grow border-none leading-6 placeholder:text-[#9fa6b2]"
-            type="text"
-            placeholder="링크를 추가해 보세요."
-          />
-        </div>
-        <button className="btn w-20 py-2 px-4 text-sm">추가하기</button>
-      </form>
-    </section>
+    <form className="flex justify-center items-center max-w-[800px] mx-auto py-4 px-5 bg-white border-purple  rounded-2xl">
+      <div className="flex justify-center items-center gap-3 flex-grow">
+        <Image src={'/assets/icons/link.svg'} width={20} height={20} alt="폴더 추가 인풋 아이콘" />
+        <input
+          className="flex-grow border-none leading-6 placeholder:text-[#9fa6b2]"
+          type="text"
+          placeholder="링크를 추가해 보세요."
+        />
+      </div>
+      <button
+        className="btn w-20 py-2 px-4 text-sm"
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+          e.preventDefault();
+          openModal('AddToFolderModal');
+          setModalFolderName('');
+        }}>
+        추가하기
+      </button>
+    </form>
   );
 }
--- src/components/folder/Option.tsx
+++ src/components/folder/Option.tsx
@@ -1,24 +1,47 @@
 import Image from 'next/image';
 
-export default function Option() {
+interface OptionProps {
+  title: string;
+  openModal: (modalName: string) => void;
+  setModalFolderName: (folderName: string) => void;
+}
+
+export default function Option({ title, openModal, setModalFolderName }: OptionProps) {
   return (
     <div className="flex justify-between items-center">
-      <h2 className="text-2xl font-semibold ">zz</h2>
+      <h2 className="text-2xl font-semibold ">{title}</h2>
 
-      <div className="flex justify-center items-center gap-3">
-        <button className="flex justify-center items-center gap-1">
-          <Image src={'/assets/icons/share.svg'} width={20} height={20} alt="공유 아이콘" />
-          <p>공유</p>
-        </button>
-        <button className="flex justify-center items-center gap-1">
-          <Image src={'/assets/icons/pen.svg'} width={20} height={20} alt="펜 아이콘" />
-          <p>이름 변경</p>
-        </button>
-        <button className="flex justify-center items-center gap-1">
-          <Image src={'/assets/icons/delete.svg'} width={20} height={20} alt="휴지통 아이콘" />
-          <p>삭제</p>
-        </button>
-      </div>
+      {title !== '전체' && (
+        <div className="flex justify-center items-center gap-3">
+          <button
+            onClick={() => {
+              openModal('FolderSharedModal');
+              setModalFolderName(title);
+            }}
+            className="flex justify-center items-center gap-1">
+            <Image src={'/assets/icons/share.svg'} width={20} height={20} alt="공유 아이콘" />
+            <p>공유</p>
+          </button>
+          <button
+            onClick={() => {
+              openModal('FolderNameChangeModal');
+              setModalFolderName(title);
+            }}
+            className="flex justify-center items-center gap-1">
+            <Image src={'/assets/icons/pen.svg'} width={20} height={20} alt="펜 아이콘" />
+            <p>이름 변경</p>
+          </button>
+          <button
+            onClick={() => {
+              openModal('FolderDeleteModal');
+              setModalFolderName(title);
+            }}
+            className="flex justify-center items-center gap-1">
+            <Image src={'/assets/icons/delete.svg'} width={20} height={20} alt="휴지통 아이콘" />
+            <p>삭제</p>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
