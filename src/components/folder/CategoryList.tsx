4b5540412c29dbe5564c4fc4a0b282e95d3ebc92
황준용 <wnsdyd3333@gmail.com>
2024-04-02 12:46:17 +0900

    feat: 카테고리 리스트 api 요청 구현

--- src/components/folder/CategoryList.tsx
+++ src/components/folder/CategoryList.tsx
@@ -1,13 +1,51 @@
+import { Folder } from '@/app/folder/page';
 import '@/app/globals.css';
 
-export default function CategoryList() {
+interface CategoryListProps {
+  categoryList: Folder[] | null;
+  setTitle: React.Dispatch<string>;
+  title: string;
+  setFolderId: React.Dispatch<number>;
+  openModal: (modalType: string) => void;
+  setModalFolderName: (title: string) => void;
+}
+
+export default function CategoryList({
+  categoryList,
+  title,
+  setTitle,
+  setFolderId,
+  openModal,
+  setModalFolderName,
+}: CategoryListProps) {
   return (
     <div className="flex justify-between items-center">
       <div className="flex flex-wrap gap-2">
-        <button className="border-purple py-2 px-3 text-black hover:text-white hover:bg-primary">전체</button>
-        {/* 데이터 받아서 넣어야 함 */}
+        <button
+          onClick={() => setTitle('전체')}
+          className="border-purple py-2 px-3 text-black hover:text-white hover:bg-primary">
+          전체
+        </button>
+        {categoryList?.map((list) => (
+          <button
+            key={list.id}
+            className={`border-purple py-2 px-3 text-black hover:text-white hover:bg-primary `}
+            onClick={() => {
+              setTitle(list.name);
+              setFolderId(list.id);
+            }}>
+            {list.name}
+          </button>
+        ))}
       </div>
-      <button className="text-primary font-medium whitespace-normal mobile:add-folder-btn">폴더 추가 +</button>
+      <button
+        onClick={() => {
+          openModal('FolderAddModal');
+          setModalFolderName(title);
+        }}
+        className="text-primary font-medium whitespace-normal mobile:add-folder-btn">
+        폴더 추가 +
+      </button>
     </div>
   );
 }
