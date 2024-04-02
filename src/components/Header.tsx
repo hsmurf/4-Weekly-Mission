03ef3c55f0cbf1ada4fa4e215fcb914a9e0ba6f6
황준용 <wnsdyd3333@gmail.com>
2024-04-02 12:47:55 +0900

    feat: 헤더 컴포넌트 api 호출 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,51 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
+import { getSampleUser, getUser } from '@/services/api';
+
+interface User {
+  email: string;
+  profileImageSource: string;
+  image_source: string;
+}
 
 export default function Header() {
+  const pathname = usePathname();
+  const isSharedPath = pathname === '/shared';
+
+  const [user, setUser] = useState<User | null>(null);
+  const [profileImage, setProfileImage] = useState<string>('');
+
+  useEffect(() => {
+    const userData = isSharedPath
+      ? getSampleUser().then((data) => setUser(data))
+      : getUser().then((data) => setUser(data));
+  }, [isSharedPath]);
+
+  useEffect(() => {
+    if (user) {
+      const imageSource = user.profileImageSource || user.image_source;
+      setProfileImage(imageSource);
+    }
+  }, [user]);
   return (
     <header className=" bg-blue-50 z-10">
-      <nav className="flex justify-between items-center w-full max-w-[1920px] h-24 px-52 tablat:px-8">
+      <nav className="flex justify-between items-center w-full max-w-[1920px] h-24 px-52 tablet:px-8">
         <Link href={'/'}>
           <Image src="/assets/icons/logo.svg" width={130} height={130} alt="로고" />
         </Link>
-        <div className="flex justify-center items-center gap-1.5">
-          <Image src={'/assets/icons/kakao.svg'} width={28} height={28} alt="프로필 이미지" />
-          <span className=" text-[14px] text-[#373740]">sss</span>
-        </div>
+        {user ? (
+          <div className="flex justify-center items-center gap-1.5">
+            <Image className="rounded-full" src={profileImage} width={28} height={28} alt="프로필 이미지" />
+            <span className=" text-[14px] text-[#373740]">{user.email}</span>
+          </div>
+        ) : (
+          <Link href={'/signin'} className="btn w-[128px]">
+            로그인
+          </Link>
+        )}
       </nav>
     </header>
   );
