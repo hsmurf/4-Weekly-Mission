a64c32e0bb02a1a99cb1847cc60094239a88bb7d
황준용 <wnsdyd3333@gmail.com>
2024-04-08 08:57:08 +0900

    feat: 로그인 회원가입 레이아웃

---
+++ src/app/(sign)/layout.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { PropsWithChildren } from 'react';
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex flex-col justify-center items-center bg-[#F0F6FF] h-screen">
+      <Link href={'/'}>
+        <Image src={'/assets/icons/logo.svg'} width={200} height={200} alt="로고" />
+      </Link>
+      {children}
+    </div>
+  );
+}
