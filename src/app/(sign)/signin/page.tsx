37cd7e24efcd55d8d1a5208c113bcf27edbb9df0
황준용 <wnsdyd3333@gmail.com>
2024-04-08 17:08:29 +0900

    feat: 로그인 페이지 로그인 유효 검증 구현 및 토큰 보유 시 자동 로그인

--- src/app/(sign)/signin/page.tsx
+++ src/app/(sign)/signin/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import { EmailInput, PasswordCheckInput, PasswordInput } from '../../../components/Inputs';
 import { useForm } from 'react-hook-form';
 import SocialBar from '@/components/SocialBar';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 interface FormData {
   email: string;
@@ -16,8 +18,37 @@ export default function Page() {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>({ mode: 'onBlur' });
+  const router = useRouter();
 
-  const onSubmit = (data: any) => data;
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      router.push('/folder');
+    }
+  }, []);
+
+  const onSubmit = async (inputData: FormData, e: any) => {
+    e.preventDefault();
+    try {
+      const signIpResponse = await fetch('https://bootcamp-api.codeit.kr/api/sign-in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: inputData.email, password: inputData.password }),
+      });
+
+      if (signIpResponse.ok) {
+        const responseData = await signIpResponse.json();
+        const { data } = responseData;
+        localStorage.setItem('accessToken', data.accessToken);
+        router.push('/folder');
+      }
+    } catch (error) {
+      throw new Error('서버 응답에 문제가 있습니다.');
+    }
+  };
   return (
     <div className="flex flex-col gap-8 w-full max-w-[400px]">
       <div className="flex justify-center items-center gap-2 mt-4">
